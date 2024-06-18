948dd4845de1c5f3bb5b663fa05b55db57e6b656
ShrihaDeo <168189930+ShrihaDeo@users.noreply.github.com>
2024-06-18 21:00:26 +1200

    About section

---
+++ app/components/utils/personal-data.js
@@ -0,0 +1,10 @@
+export const personalData = {
+    name: "Shriha Deo",
+    profile: '/profile.png',
+    designation: "Software Developer",
+    description: "My name is Shriha Deo. I am an enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+    email: 'shrihadeo009@gmail.com',
+    phone: '+640210366310',
+    github: 'https://github.com/ShrihaDeo',
+    linkedIn: 'https://www.linkedin.com/in/shriha-deo-386b662b6/',
+  }
---
+++ app/directory/page.tsx
@@ -0,0 +1,10 @@
+import { personalData } from '../components/utils/personal-data';
+import AboutSection from '../components/About/about';
+
+export default async function Home() {  
+    return (
+      <>
+        <AboutSection />
+      </>
+    )
+  };
