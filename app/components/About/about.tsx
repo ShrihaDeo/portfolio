0d3050e69836c544ffcfa48351b07840a9ad0f51
ShrihaDeo <168189930+ShrihaDeo@users.noreply.github.com>
2024-06-19 19:14:55 +1200

    Updates

--- app/components/About/about.tsx
+++ app/components/About/about.tsx
@@ -1,4 +1,4 @@
-'use client'
+
 import { personalData } from '../utils/personal-data';
 import pic from '../utils/profile.png';
 import Image from "next/image";
@@ -10,36 +10,20 @@ function AboutSection() {
       className="my-12 lg:my-16 relative"
       style={{
         background: 'linear-gradient(to right, #a1c4fd, #fbc2eb)',
-        padding: '20px',
-        borderRadius: '10px'
+        padding: '150px',
+        borderRadius: '10px',
       }}
     >
-      <div className="hidden lg:flex flex-col items-center absolute top-16 -right-8">
-        <span
-          style={{
-            backgroundColor: '#1a1443',
-            color: 'white',
-            transform: 'rotate(90deg)',
-            padding: '10px 20px',
-            fontSize: '20px',
-            borderRadius: '10px',
-            fontFamily: 'Courier New, Courier, monospace'
-          }}
-        >
-          ABOUT ME
-        </span>
-        <span className="h-36 w-[2px] bg-[#1a1443]"></span>
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
         <div className="order-2 lg:order-1">
           <p
             style={{
               fontWeight: 'bold',
               marginBottom: '20px',
               color: '#00008b', // Dark Blue
-              fontSize: '20px',
+              fontSize: '50px',
               textTransform: 'uppercase',
-              fontFamily: 'Courier New, Courier, monospace'
+              fontFamily: 'Courier New, Courier, monospace',
             }}
           >
             Who I am?
@@ -47,8 +31,8 @@ function AboutSection() {
           <p
             style={{
               color: '#00008b', // Dark Blue
-              fontSize: '16px',
-              fontFamily: 'Courier New, Courier, monospace'
+              fontSize: '25px',
+              fontFamily: 'Courier New, Courier, monospace',
             }}
           >
             {personalData.description}
@@ -56,9 +40,9 @@ function AboutSection() {
         </div>
         <div className="flex justify-center order-1 lg:order-2">
           <Image
-            src= { pic }
-            width={280}
-            height={280}
+            src={pic}
+            width={380}
+            height={380}
             alt="Shriha Deo"
             className="rounded-lg transition-all duration-1000 grayscale hover:grayscale-0 hover:scale-110 cursor-pointer"
           />
@@ -69,4 +53,5 @@ function AboutSection() {
 }
 
 export default AboutSection;
+
 
--- app/components/utils/personal-data.js
+++ app/components/utils/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
     name: "Shriha Deo",
     profile: '/profile.png',
     designation: "Software Developer",
-    description: "My name is Shriha Deo. I am an enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+    description: "I'm an avid reader and programmer, driven by curiosity and a passion for technology. I thrive on learning new concepts and am constantly exploring the latest advancements in STEM fields, particularly in web application development. As a woman in STEM, I'm committed to breaking barriers and contributing to a more inclusive and diverse tech industry. One of my core strengths lies in JavaScript, and I enjoy leveraging its versatility to create innovative solutions. I'm deeply interested in making the web more accessible and user-friendly, aiming to contribute positively to the digital landscape. Additionally, I'm an enthusiastic gamer, recognizing the value of gaming in fostering creativity, problem-solving skills, and teamwork. I'm actively seeking job opportunities that align with my skills, interests, and values, eager to make meaningful contributions to projects that excite me.",
     email: 'shrihadeo009@gmail.com',
     phone: '+640210366310',
     github: 'https://github.com/ShrihaDeo',
